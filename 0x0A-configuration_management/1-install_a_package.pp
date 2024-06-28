1ad8b0962d416ddc6d4a280b128021d3e3ca21bc
bugfix.exe <bugfix.exe@gmai.com>
2024-06-28 07:25:10 -0700

    debugging problem 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,12 @@
 ## Specifically Flask
 package {'Flask':
       ensure => '2.1.0',
+      name   => 'flask',
     provider => 'pip3',
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  name     => 'Werkzeug',
+  provider => 'pip3',
 }
